1f7eb076455845f68605037fb33546ae80e70df4
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-04-17 11:49:08 -0600

    Time: 45 ms (6.69%) | Memory: 16.7 MB (39.94%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -6,9 +6,5 @@ class Solution:
         while j < len(t) and i < len(s):
             if s[i] == t[j]:
                 i += 1
-                j += 1
-            else:
-                j += 1
-        if i == len(s):
-            return True
-        return False
+            j += 1
+        return i == len(s)
