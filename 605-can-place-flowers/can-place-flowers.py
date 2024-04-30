adfc218c2541eaedb78fceba8ed4ad1b2691ea7e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-04-29 19:31:51 -0600

    Time: 127 ms (80.63%) | Memory: 16.8 MB (72.95%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -2,19 +2,13 @@ class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         empty = 0 if flowerbed[0] else 1
 
-        for f in flowerbed:
-            if f:
+        for i in flowerbed:
+            if i:
                 n -= int((empty - 1) / 2)
                 empty = 0
             else:
                 empty += 1
-            
+
         n -= (empty) // 2
         return n <= 0
                 
-
-
-            
-
-        
-
