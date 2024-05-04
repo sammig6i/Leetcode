3f5333d5256f2cbfa94fd4e3bf927ea6eac376c3
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-05-04 14:28:06 -0600

    Time: 38 ms (54.73%) | Memory: 16.7 MB (51.17%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        countMap = {char: text.count(char) for char in set(text)}
-        balloon = "balloon"
-        balloonMap = {char: balloon.count(char) for char in set(balloon)}
+        countMap = Counter(text)
+        balloonMap = Counter("balloon")
 
         res = len(text)
         for c in balloonMap:
-            res = min(res, countMap.get(c, 0) // balloonMap[c])
+            res = min(res, countMap[c] // balloonMap[c])
         return res
 
 
