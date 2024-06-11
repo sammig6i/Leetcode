46c909591d1d2d86128476d69cfa80c1e9c7d4a7
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-06-11 15:53:18 -0600

    Time: 61 ms (81.05%) | Memory: 17 MB (95.66%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -3,21 +3,19 @@ class Solution:
         stack = []
 
         for c in tokens:
-            if c in {'+', '-', '/', '*'}:
-                b = stack.pop() 
-                a = stack.pop()
-                if c == '+':
+            if c in {"+", "-", "/", "*"}:
+                b = int(stack.pop())
+                a = int(stack.pop())
+
+                if c == "+":
                     result = a + b
-                    stack.append(result)
-                elif c == '-':
+                elif c == "-":
                     result = a - b
-                    stack.append(result)
-                elif c == '*':
+                elif c == "*":
                     result = a * b
-                    stack.append(result)
-                elif c == '/':
+                elif c == "/":
                     result = int(a / b)
-                    stack.append(result)
+                stack.append(result)
             else:
                 stack.append(int(c))
         return stack[0]
