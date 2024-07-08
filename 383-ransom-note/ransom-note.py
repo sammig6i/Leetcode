0c85006cf64dd67b12897d48520424975233a0bd
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-08 11:47:32 -0600

    Time: 48 ms (83.31%) | Memory: 16.7 MB (28.69%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,13 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        magMap = {}
-        for c in magazine:
-            magMap[c] = 1 + magMap.get(c, 0)
-        
-        for c in ransomNote:
-            if c in magMap and magMap[c] > 0:
-                magMap[c] -= 1
-            elif c not in magMap or magMap[c] == 0:
-                return False
-
-        return True
+        return Counter(magazine) >= Counter(ransomNote)
