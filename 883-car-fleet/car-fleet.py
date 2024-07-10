c8cdfd07b4611ab27b49df72c8bba19471fb5f37
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-09 23:24:36 -0600

    Time: 753 ms (26.62%) | Memory: 35.8 MB (92.88%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,8 +1,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pairs = [(p, s) for p, s in zip(position, speed)]
-
+        pairs = [[p, s] for p, s in zip(position, speed)]
         pairs.sort()
+
         stack = []
 
         for p, s in pairs[::-1]:
@@ -11,7 +11,3 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
-
-
-
-
