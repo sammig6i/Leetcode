39bb88db65e3123a6df9208f3b92aafccbf4d1c5
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-11 11:59:51 -0600

    Time: 89 ms (94.24%) | Memory: 17.3 MB (57.82%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,13 +1,15 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): return ""
+        if len(t) > len(s): return ""
+
         window, countT = {}, {}
 
         for c in t:
             countT[c] = 1 + countT.get(c, 0)
         
-        have, need = 0, len(countT)
         res, resLen = [-1, -1], float('inf')
+        have, need = 0, len(countT)
+
         L = 0
         for R in range(len(s)):
             c = s[R]
@@ -19,9 +21,11 @@ class Solution:
                 if (R - L + 1) < resLen:
                     res = [L, R]
                     resLen = (R - L + 1)
-                window[s[L]] -= 1
-                if s[L] in countT and window[s[L]] < countT[s[L]]:
+                c = s[L]
+                window[c] -= 1
+                if c in countT and window[c] < countT[c]:
                     have -= 1
                 L += 1
         L, R = res
-        return s[L:R + 1] if resLen != float('inf') else ""
+        return s[L: R + 1] if resLen != float('inf') else ""
+
