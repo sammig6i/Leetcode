9a2819142100cda99050d4acf895b8cfdd14fcf8
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-11 11:30:50 -0600

    Time: 52 ms (15.63%) | Memory: 16.9 MB (88.25%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,11 +1,13 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
 
         top, bot = 0, ROWS - 1
 
         while top <= bot:
             row = (top + bot) // 2
+
 
             if target > matrix[row][-1]:
                 top += 1
@@ -17,12 +19,12 @@ class Solution:
         L, R = 0, COLS - 1
 
         while L <= R:
-            m = L + (R - L) // 2
+            m = L + ((R - L) // 2)
 
-            if matrix[row][m] > target:
-                R -= 1
-            elif matrix[row][m] < target:
+            if target > matrix[row][m]:
                 L += 1
+            elif target < matrix[row][m]:
+                R -= 1
             else:
                 return True
         return False
