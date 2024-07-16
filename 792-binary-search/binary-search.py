69e78b59f7696aa33b892da987378c9fbe4c5348
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-16 14:06:29 -0600

    Time: 182 ms (92.45%) | Memory: 18.2 MB (27.81%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,12 +3,12 @@ class Solution:
         L, R = 0, len(nums) - 1
 
         while L <= R:
-            mid = L + ((R - L) // 2)
+            m = L + ((R - L) // 2)
 
-            if target > nums[mid]:
-                L = mid + 1
-            elif target < nums[mid]:
-                R = mid - 1
+            if nums[m] < target:
+                L = m + 1
+            elif nums[m] > target:
+                R = m - 1
             else:
-                return mid
+                return m
         return -1
