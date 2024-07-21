a730ce6d2779631b129f18ceff916e5d3b980234
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-20 21:12:53 -0600

    Time: 252 ms (96.39%) | Memory: 25.6 MB (73.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,6 +1,7 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * len(nums)
+
         pre = 1
         for i in range(len(nums)):
             res[i] = pre
@@ -10,4 +11,5 @@ class Solution:
         for i in range(len(nums) - 1, -1, -1):
             res[i] *= post
             post *= nums[i]
+
         return res
