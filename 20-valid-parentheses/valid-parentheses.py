579e43729fb009d920bbe67a1022b03bef1b484c
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-07-30 22:41:38 -0600

    Time: 34 ms (68.10%) | Memory: 16.5 MB (42.39%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,8 +4,8 @@ class Solution:
         stack = []
 
         for c in s:
-            if stack and c in closedToOpen:
-                if closedToOpen[c] == stack[-1]:
+            if c in closedToOpen:
+                if stack and closedToOpen[c] == stack[-1]:
                     stack.pop()
                 else:
                     return False
