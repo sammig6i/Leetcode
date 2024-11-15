7ee754de9a86d61b17d562aa12a64e198f5c3c71
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-14 23:38:30 -0700

    Time: 0 ms (100.00%) | Memory: 17.4 MB (33.67%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -16,13 +16,11 @@ class Solution:
         while len(q) > 0:
             level = []
             for i in range(len(q)):
-                curr = q.popleft()
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    q.append(curr.right)
-                if curr:
-                    level.append(curr.val)
+                node = q.popleft()
+                if node:
+                    q.append(node.left)
+                    q.append(node.right)
+                    level.append(node.val)
             if level:
                 res.append(level)
         return res
