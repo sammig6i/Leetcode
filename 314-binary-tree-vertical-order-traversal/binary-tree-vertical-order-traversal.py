f481e2b80c864a39925164916ec12a0ec084993c
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-16 17:28:27 -0700

    Time: 0 ms (100.00%) | Memory: 16.9 MB (14.75%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -8,22 +8,30 @@ class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-
+        
         columnTable = defaultdict(list)
         min_column = max_column = 0
-        q = deque([(root, 0)])
 
-
-        while q:
-            node, column = q.popleft()
+        def DFS(node, row, column):
             if node:
-                columnTable[column].append(node.val)
+                nonlocal min_column, max_column
+                columnTable[column].append((row, node.val))
                 min_column = min(min_column, column)
                 max_column = max(max_column, column)
 
-                q.append((node.left, column - 1))
-                q.append((node.right, column + 1))
-        return [columnTable[v] for v in range(min_column, max_column + 1)]
+                DFS(node.left, row + 1, column - 1)
+                DFS(node.right, row + 1, column + 1)
+        
+        DFS(root, 0, 0)
+
+        res = []
+        for col in range(min_column, max_column + 1):
+            columnTable[col].sort(key=lambda x:x[0])
+            colVals = [val for row, val in columnTable[col]]
+            res.append(colVals)
+
+        return res
+                
 
 
 
