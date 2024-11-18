77123f7e3f73f1bd2484539aebe27bb63ad8585e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-18 01:34:40 -0700

    Time: 50 ms (52.48%) | Memory: 20.2 MB (24.25%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,22 +10,10 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        p_depth = self.get_depth(p)
-        q_depth = self.get_depth(q)
-        for _ in range(p_depth - q_depth):
+        path = set()
+        while p:
+            path.add(p)
             p = p.parent
-        for _ in range(q_depth - p_depth):
+        while q not in path:
             q = q.parent
-        
-        while p != q:
-            p = p.parent
-            q = q.parent
-        return p
-
-    
-    def get_depth(self, node):
-        depth = 0
-        while node:
-            node = node.parent
-            depth += 1
-        return depth
+        return q
