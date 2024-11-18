efb9bfe65984e8757ebafea8bf6afeb4d33c65cf
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-17 20:59:33 -0700

    Time: 75 ms (49.94%) | Memory: 20.9 MB (27.31%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,35 +1,21 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # parse each number in the string
-        # take care of order of operations according to pemdas: *, /, +, -
-        # if * or / in stack, then immediately calculate the two numbers
+        op = '+'
         stack = []
         v = 0
-        op = "+"
-
         for i, c in enumerate(s):
             if c.isdigit():
                 v = v * 10 + int(c)
-
-            if i == len(s) - 1 or c in ('+','-','*','/'):
+            if i == len(s) - 1 or c in ("+","-","/","*"):
                 if op == "+":
-                    stack.append(v)
+                    stack.append(v)    
                 elif op == "-":
                     stack.append(-v)
                 elif op == "*":
                     stack.append(stack.pop() * v)
                 elif op == "/":
                     stack.append(int(stack.pop() / v))
-                
                 op = c
                 v = 0
         return sum(stack)
-
-
-
-        
-            
-
-
-
-        
+                
