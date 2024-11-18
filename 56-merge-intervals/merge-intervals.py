b4b20e9929f19c51f3e3d3f9615128b203653eb1
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-17 19:21:59 -0700

    Time: 0 ms (100.00%) | Memory: 20.8 MB (33.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,15 +3,12 @@ class Solution:
         intervals.sort(key=lambda x: x[0])
         res = []
         for start, end in intervals:
-            if res:
+            if not res:
+                res.append([start, end])
+            else:
                 last_end = res[-1][1]
                 if start <= last_end:
-                    if end <= last_end:
-                        continue
-                    else:
-                        res[-1][1] = end
+                    res[-1][1] = max(last_end, end)
                 else:
                     res.append([start, end])
-            else:
-                res.append([start, end])
         return res
