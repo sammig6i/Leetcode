9d0a2e23ec8b5305e1af4b799ee93cc734014894
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-19 14:06:40 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (33.50%) - LeetSync

--- 339-nested-list-weight-sum/nested-list-weight-sum.py
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -43,19 +43,21 @@
 
 class Solution:
     def depthSum(self, nestedList: List[NestedInteger]) -> int:
-        # when iterating through nestedList update the multiple = depth of current iteration
-        return self.dfs(nestedList, 1)
+        depth = 1
+        return self.dfs(nestedList, depth)
+        
 
-    def dfs(self, nested_list, depth):
-        res = 0
-        for item in nested_list:
-            if item.isInteger():
-                res += (item.getInteger() * depth)
+    def dfs(self, nestedList, depth):
+        depth_sum = 0
+        for nestedList in nestedList:
+            if nestedList.isInteger():
+                depth_sum += (nestedList.getInteger() * depth)
             else:
-                res += self.dfs(item.getList(), depth + 1)
-        return res
+                depth_sum += self.dfs(nestedList.getList(), depth + 1)
+        return depth_sum
+        
 
 
 
 
-
+        
