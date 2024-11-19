6087e8f545794f05bc62897437613874089dd156
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-19 12:22:07 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (45.14%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -6,45 +6,15 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d = defaultdict(list)
+        q = deque([(root, 0)])
         if not root:
             return []
-        # hash mapy will store keys -> column, values -> node values
-        columnTable = defaultdict(list)
-        # min and max column will be used for range when returning answer
-        min_column = max_column = 0
-        # make DFS helper
-        def DFS(node, row, column):
+
+        while q:
+            node, column = q.popleft()
             if node:
-                # add nonlocal to be able to modify the min and max gloabl variables
-                nonlocal min_column, max_column
-                # process the current node by appending the (row, node value) in column key
-                columnTable[column].append((row, node.val))
-                # update min column
-                min_column = min(min_column, column)
-                # do the same for max column
-                max_column = max(max_column, column)
-
-                # preorder traveral (can use inorder or postorder as well)
-                DFS(node.left, row + 1, column - 1)
-                DFS(node.right, row + 1, column + 1)
-        # being DFS
-        DFS(root, 0, 0)
-        # sort each (row, node value) tuple for each of their respective column key in hashtable
-        res = []
-        for col in range(min_column, max_column + 1):
-            # sort the column key by row which is the first position in each tuple
-            columnTable[col].sort(key=lambda x:x[0])
-            # collect the values
-            colVals = [val for _, val in columnTable[col]]
-            res.append(colVals)
-
-        return res
-                
-
-
-
-
-
-
-
-
+                q.append([node.left, column - 1])
+                d[column].append(node.val)
+                q.append([node.right, column + 1])
+        return [v for _, v in sorted(d.items())]
