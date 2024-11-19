8acf182d3716be31144e7e2395e819360365364f
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-19 14:20:37 -0700

    Time: 43 ms (96.53%) | Memory: 21.2 MB (16.30%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,35 +1,23 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        curr_res = 0
-        res = 0
-        num = 0
-        op = "+"  # keep the last operator we have seen
-        
-		# append a "+" sign at the end because we can catch the very last item
-        for ch in s + "+":
-            if ch.isdigit():
-                num = 10 * num + int(ch)
+        v = 0
+        stack = []
+        op = "+"
+        # res = 0
 
-            # if we have a symbol, we would start to calculate the previous part.
-            # note that we have to catch the last chracter since there will no sign afterwards to trigger calculation
-            if ch in ("+", "-", "*", "/"):
+        for c in s + "+":
+            if c.isdigit():
+                v = v * 10 + int(c)
+            elif c in ("+", "*", "/", "-"):
                 if op == "+":
-                    curr_res += num
+                    stack.append(v)
                 elif op == "-":
-                    curr_res -= num
-                elif op == "*":
-                    curr_res *= num
+                    stack.append(-v)
                 elif op == "/":
-                    # in python if there is a negative number, we should alway use int() instead of //
-                    curr_res = int(curr_res / num)
-                
-                # if the chracter is "+" or "-", we do not need to worry about
-                # the priority so that we can add the curr_res to the eventual res
-                if ch in ("+", "-"):
-                    res += curr_res
-                    curr_res = 0
-                
-                op = ch
-                num = 0
-        
-        return res
+                    stack.append(int(stack.pop() / v))
+                elif op == "*":
+                    stack.append(stack.pop() * v)
+                op = c
+                v = 0
+
+        return sum(stack)
