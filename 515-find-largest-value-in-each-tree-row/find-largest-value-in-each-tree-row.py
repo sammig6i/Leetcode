147e75f086d4bfa58dd8d010dec6afd0f988931f
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-20 15:47:59 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (23.30%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -6,36 +6,20 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
         res = []
+        q = deque([root])
 
-        def dfs(node, depth):
-            if not node:
-                return None
-            if len(res) == depth:
-                res.append(node.val)
-            else:
-                res[depth] = max(res[depth], node.val)
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
-        
-        dfs(root, 0)
+        while q:
+            max_node = float("-inf")
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    if node.left: q.append(node.left)
+                    if node.right: q.append(node.right)
+                    max_node = max(node.val, max_node)
+            res.append(max_node)
+
         return res
 
-
-
-
-        # q = deque([root])
-        # res = []
-        # if not root:
-        #     return []
-
-        # while q:
-        #     max_node = float("-inf")
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         if node:
-        #             max_node = max(max_node, node.val)
-        #             if node.left: q.append(node.left)
-        #             if node.right: q.append(node.right)
-        #     res.append(max_node)
-        # return res
