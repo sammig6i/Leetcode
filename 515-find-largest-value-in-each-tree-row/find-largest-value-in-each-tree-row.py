f6b8b26469428a08286c03536f26724f1fec636e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-20 15:20:31 -0700

    Time: 4 ms (19.88%) | Memory: 19 MB (23.30%) - LeetSync

---
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        q = deque([root])
+        res = []
+        if not root:
+            return []
+
+        while q:
+            max_node = float("-inf")
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    max_node = max(max_node, node.val)
+                    if node.left: q.append(node.left)
+                    if node.right: q.append(node.right)
+            res.append(max_node)
+        return res
