c30b7b146acfbdecc8ba5d7e48686b165f392d33
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-20 12:32:25 -0700

    Time: 179 ms (66.04%) | Memory: 18.6 MB (7.09%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,22 +1,22 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        N = len(grid)
-        # r, c
-        visited = set([(0, 0)])
-        # row, col, length
+        # r, c, length
         q = deque([(0, 0, 1)])
-        direction = [[0, 1], [1, 0], [0, -1], [-1, 0],
-                    [1, 1], [-1, -1], [-1, 1], [1, -1]]
-        
+        # r, c
+        visited = set((0, 0))
+        direct = [[0, 1], [1, 0], [0, -1], [-1, 0],
+                [1, 1], [-1, -1], [1, -1], [-1, 1]]
+        N = len(grid)
+
         while q:
             r, c, length = q.popleft()
-            if (min(r, c) < 0 or max(r, c) >= N
-                or grid[r][c]):
+            if min(r, c) < 0 or max(r, c) >= N or grid[r][c]:
                 continue
             if r == N - 1 and c == N - 1:
                 return length
-            for dr, dc in direction:
+            for dr, dc in direct:
                 if (r + dr, c + dc) not in visited:
                     q.append((r + dr, c + dc, length + 1))
                     visited.add((r + dr, c + dc))
         return -1
+        
