a972a117e1acb90c5bd51d467a5c3258b9022ddd
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-20 13:06:09 -0700

    Time: 50 ms (87.45%) | Memory: 18 MB (82.18%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -8,20 +8,19 @@ class Solution:
         for i, c in enumerate(s + "+"):
             if c.isdigit():
                 v = v * 10 + int(c)
-            if c in ("+", "*", "/", "-"):
+            elif c in ("+", "-", "/", "*"):
                 if op == "+":
                     curr_sum += v
                 elif op == "-":
                     curr_sum -= v
-                elif op == "/":
-                    curr_sum = int(curr_sum / v)
                 elif op == "*":
                     curr_sum *= v
-                
+                elif op == "/":
+                    curr_sum = int(curr_sum / v)
+
                 if c in ("+", "-"):
                     res += curr_sum
                     curr_sum = 0
                 op = c
                 v = 0
-                
         return res
