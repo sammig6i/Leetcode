597194463310150487c361f17ae27cad809d7b94
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-21 14:40:52 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (10.98%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,24 +1,29 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        if not root:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if root is None:
             return []
-        
-        def dfs(root, depth):
-            if not root:
-                return None
-            
-            if len(res) == depth:
-                res.append(root.val)
-            
-            dfs(root.right, depth + 1)
-            dfs(root.left, depth + 1)
-        
-        dfs(root, 0)
-        return res
+
+        queue = deque(
+            [
+                root,
+            ]
+        )
+        rightside = []
+
+        while queue:
+            level_length = len(queue)
+
+            for i in range(level_length):
+                node = queue.popleft()
+
+                # if it's the rightmost element
+                if i == level_length - 1:
+                    rightside.append(node.val)
+
+                # add child nodes in the queue
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return rightside
