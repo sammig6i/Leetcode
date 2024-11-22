868d8fca9e7aaaf49ab1e33f7c158a15ec8bf4c0
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-22 15:44:41 -0700

    Time: 39 ms (66.91%) | Memory: 19.6 MB (13.05%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -4,15 +4,15 @@ class Solution:
         stack = []
 
         for c in s:
-            if c == "(":
-                stack.append(c)
+            if c == ")" and count == 0:
+                continue
+            elif c == "(":
                 count += 1
-            elif c == ")" and count > 0:
-                count -= 1
-                stack.append(c)
             # we do not want -> count == ) and count == 0
-            elif c != ")":
-                stack.append(c)
+            elif c == ")":
+                count -= 1
+            stack.append(c)
+
         filtered = []
         for c in stack[::-1]:
             if c == "(" and count > 0:
