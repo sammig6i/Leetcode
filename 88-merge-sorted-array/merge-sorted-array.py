1403cf18b8c0984697de5ba16b835cdc9794801f
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-22 17:06:27 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (55.07%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -5,7 +5,7 @@ class Solution:
         """
         # Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
         last = m + n - 1
-        while n > 0 and m > 0:
+        while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
                 m -= 1
@@ -13,14 +13,11 @@ class Solution:
                 nums1[last] = nums2[n - 1]
                 n -= 1
             last -= 1
-            
+        
         while n > 0:
             nums1[last] = nums2[n - 1]
             n -= 1
             last -= 1
-        return nums1
         
-# [0] m = 0 last = 0
-# [1] n = 1
-
+        return nums1
  
