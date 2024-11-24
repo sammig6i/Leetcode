5b7679d51c8f8592707d6c6be3454ff823d557ab
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-24 14:54:40 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (10.65%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,28 +3,42 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = len(nums) - 2
-        while i >= 0 and nums[i + 1] <= nums[i]:
-            i -= 1
-        if i >= 0:
-            j = len(nums) - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            self.swap(nums, i, j)
-        self.reverse(nums, i + 1)
+        # 3 1 2 -> 3 2 1
+        # pivot = 1, 3 2 1
 
-    def reverse(self, nums, start):
-        i, j = start, len(nums) - 1
-        while i < j:
-            self.swap(nums, i, j)
-            i += 1
-            j -= 1
+        # 1 3 2 -> 2 1 3
+        # pivot = 0, 
+        # 1. 2 3 1
+        # 2. 
 
-    def swap(self, nums, i, j):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
+        # 1 1 5 -> 1 5 1
+        pivot = -1
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                pivot = i
+                break
+            
+        if pivot == -1:
+            nums.reverse()
+            return
+        
+        for i in range(len(nums) - 1, pivot, -1):
+            if nums[i] > nums[pivot]:
+                nums[i], nums[pivot] = nums[pivot], nums[i]
+                break
 
+        nums[pivot + 1:] = reversed(nums[pivot + 1:])
+        
+        
+        
+        
+
+
+
+        
+        
+
+        
 
 
 
