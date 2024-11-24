2214f7323122f7df64b25842c509d510cb67bd22
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-24 16:27:49 -0700

    Time: 11 ms (32.24%) | Memory: 21.4 MB (6.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,20 +1,28 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        d = defaultdict(int)
-        for start, end in intervals:
-            d[start] += 1
-            d[end] -= 1
-        res = []
-        interval = []
-        have = 0
+        max_val = max(interval[0] for interval in intervals)
 
-        for i in sorted(d):
-            if not interval:
-                interval.append(i)
-            have += d[i]
-            if have == 0:
-                interval.append(i)
-                res.append(interval)
-                interval = []
-                have = 0
+        mp = [0] * (max_val + 1)
+
+        for start, end in intervals:
+            mp[start] = max(end + 1, mp[start])
+        
+        res = []
+        have = -1
+        interval_start = -1
+
+        for i in range(len(mp)):
+            if mp[i] != 0:
+                if interval_start == -1:
+                    interval_start = i
+                have = max(mp[i] - 1, have)
+            if have == i:
+                res.append([interval_start, have])
+                interval_start = -1
+                have = -1
+        
+        if interval_start != -1:
+            res.append([interval_start, have])
         return res
+                # 1,3 2,6
+
