5f285b62b475fd3fb3e6a8085545292ca8b24556
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-26 01:21:47 -0700

    Time: 75 ms (57.55%) | Memory: 28.3 MB (43.25%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,33 +1,12 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
 
-
-        def quickSelect(start, end, k_smallest):
-            if start == end:
-                return nums[start]
-            pivot = (start + end) // 2
-
-            pivot_val = nums[pivot]
-            L, R = start, end
-
-            while True:
-                while L <= end and nums[L] < pivot_val:
-                    L += 1
-                while R >= start and nums[R] > pivot_val:
-                    R -= 1
-                if L >= R:
-                    break  # Pointers have crossed, partitioning is done
-                nums[L], nums[R] = nums[R], nums[L]
-                L += 1
-                R -= 1
-            
-            if R < k_smallest:
-                return quickSelect(R + 1, end, k_smallest)
-            return quickSelect(start, R, k_smallest)
-
-        n = len(nums)
-        k_smallest = n - k
-
-        return quickSelect(0, n - 1, k_smallest)
-        
-
+        for n in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, n)
+            else:
+                if n > heap[0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, n)
+        return heap[0]
