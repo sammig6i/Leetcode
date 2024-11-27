5498d07764e3dc88608f0abec5771df4ef024d04
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-27 03:07:42 -0700

    Time: 31 ms (91.40%) | Memory: 18.1 MB (23.96%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -15,8 +15,6 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        # if head in self.map:
-            # return self.map[head]
         
         copy = Node(head.val)
         self.map[head] = copy
