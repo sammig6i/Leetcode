b38caf885c988a5e086340d62948efdd1ba257eb
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-27 17:49:05 -0700

    Time: 40 ms (42.89%) | Memory: 18.2 MB (23.88%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -8,36 +8,27 @@ class Node:
 """
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if head is None:
-            return None
-        
-        # Original: Node1 -> Node2 -> Node3
-        # Woven:    Node1 -> Copy1 -> Node2 -> Copy2 -> Node3 -> Copy3
-        l1 = head
-        while l1:
-            l2 = Node(l1.val)
-            l2.next = l1.next
-            l1.next = l2
-            l1 = l2.next
-        
-        l1 = head
-        while l1:
-            if l1.random:
-                l1.next.random = l1.random.next
-            l1 = l1.next.next
-        
-        newHead = head.next
+    def __init__(self):
+        self.map = {}
 
-        l1 = head
-        while l1:
-            l2 = l1.next
-            l1.next = l2.next
-            if l2.next:
-                l2.next = l2.next.next
-            l1 = l1.next
+
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        copy = Node(head.val)
+        self.map[head] = copy
+        copy.next = self.copyRandomList(head.next)
+        copy.random = self.map.get(head.random)
+        return copy
+
         
-        return newHead
+
+
+        
+        
+        
+        
         
 
             
