1bf5454588ebc780c45d0d50620aa3d3856ae03d
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-27 17:46:00 -0700

    Time: 38 ms (54.79%) | Memory: 17.9 MB (23.88%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -12,6 +12,8 @@ class Solution:
         if head is None:
             return None
         
+        # Original: Node1 -> Node2 -> Node3
+        # Woven:    Node1 -> Copy1 -> Node2 -> Copy2 -> Node3 -> Copy3
         l1 = head
         while l1:
             l2 = Node(l1.val)
@@ -19,17 +21,14 @@ class Solution:
             l1.next = l2
             l1 = l2.next
         
-        newHead = head.next
-        
-        # Original: Node1 -> Node2 -> Node3
-        # Woven:    Node1 -> Copy1 -> Node2 -> Copy2 -> Node3 -> Copy3
-
         l1 = head
         while l1:
             if l1.random:
                 l1.next.random = l1.random.next
             l1 = l1.next.next
         
+        newHead = head.next
+
         l1 = head
         while l1:
             l2 = l1.next
@@ -37,7 +36,12 @@ class Solution:
             if l2.next:
                 l2.next = l2.next.next
             l1 = l1.next
+        
         return newHead
+        
+
+            
+
                 
 
 
