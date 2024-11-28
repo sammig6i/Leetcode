806d6e649878221e33aa3691e86a6025494361a0
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-27 22:51:53 -0700

    Time: 12 ms (19.82%) | Memory: 17.4 MB (35.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,20 +3,19 @@ class Solution:
         L, R = 0, len(s) - 1
 
         while L < R:
-            if not self.isAlpha(s[L]):
-                L += 1
-            elif not self.isAlpha(s[R]):
-                R -= 1
-            else:
-                if s[L].lower() != s[R].lower():
-                    return False
-                else:
+            if s[L].lower() != s[R].lower():
+                if not self.isAlpha(s[L].lower()):
                     L += 1
+                elif not self.isAlpha(s[R].lower()):
                     R -= 1
+                else:
+                    return False
+            else:
+                L += 1
+                R -= 1
         return True
 
-
     def isAlpha(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or 
-        ord('0') <= ord(c) <= ord('9') or
-        ord('a') <= ord(c) <= ord('z'))
+        return (ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+
