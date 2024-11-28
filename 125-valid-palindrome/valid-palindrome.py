907dc809448d65722903141ebae7499874f7d8cd
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-27 23:11:00 -0700

    Time: 3 ms (98.73%) | Memory: 17.8 MB (29.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        L, R = 0, len(s) - 1
-
-        while L < R:
-            if s[L].lower() != s[R].lower():
-                if not self.isAlpha(s[L].lower()):
-                    L += 1
-                elif not self.isAlpha(s[R].lower()):
-                    R -= 1
-                else:
-                    return False
-            else:
-                L += 1
-                R -= 1
-        return True
-
-    def isAlpha(self, c):
-        return (ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
+        new_str = ''
+        for c in s:
+            if c.isalnum():
+                new_str += c.lower()
+        return new_str == new_str[::-1]
 
