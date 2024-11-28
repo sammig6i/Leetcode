0f3fc4d4e42555e99be4efe7956793bb1368536d
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-28 01:01:21 -0700

    Time: 120 ms (79.46%) | Memory: 78.1 MB (34.52%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,50 +2,49 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.next = self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-
-        self.head, self.tail = Node(0, 0), Node(0, 0)
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
         self.head.next, self.tail.prev = self.tail, self.head
-
+    
     def insert(self, node):
         prev, nxt = self.tail.prev, self.tail
-        node.next, node.prev = nxt, prev
-        prev.next, nxt.prev = node, node
-
-        # 1 - 2 <- 3 -> - tail
-
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, self.tail
+    
     def remove(self, node):
-        prev_node, next_node = node.prev, node.next
-        prev_node.next, next_node.prev = next_node, prev_node
-        
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val      
-        return -1  
+            return self.cache[key].val
+        return -1
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
+            self.cache[key].val = value
             self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.capacity:
+            self.insert(self.cache[key])
+            return
+        
+        if len(self.cache) >= self.capacity:
             lru = self.head.next
             self.remove(lru)
             del self.cache[lru.key]
-
-# a ->10, b->12, c->15 tail capacity = 4
-# put(d, 20) 
-
+        
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
