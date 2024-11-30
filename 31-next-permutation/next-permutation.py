d323907c83934780b49720120dec224240977400
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-30 11:43:42 -0700

    Time: 0 ms (100.00%) | Memory: 17.2 MB (13.98%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,42 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # 3 1 2 -> 3 2 1
-        # pivot = 1, 3 2 1
-
-        # 1 3 2 -> 2 1 3
-        # pivot = 0, 
-        # 1. 2 3 1
-        # 2. 
-
-        # 1 1 5 -> 1 5 1
         pivot = -1
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
                 pivot = i
+                # nums[i], nums[i + 1] = nums[i + 1], nums[i]
                 break
-            
+
         if pivot == -1:
-            nums.reverse()
-            return
-        
+            nums.sort()
+            return nums
+
+        # 132 -> 213
+        # 231
         for i in range(len(nums) - 1, pivot, -1):
             if nums[i] > nums[pivot]:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
                 break
-
         nums[pivot + 1:] = reversed(nums[pivot + 1:])
-        
-        
-        
-        
-
-
-
-        
-        
-
-        
-
-
-
+        return nums
