0ca2f830fc2737beed38bcfa92bad25eeb6e4453
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-01 16:31:10 -0700

    Time: 54 ms (28.95%) | Memory: 31.2 MB (30.95%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -2,8 +2,9 @@ class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         if not heights:
             return []
+        
         res = []
-    
+
         for i, h in enumerate(heights):
             while res and heights[res[-1]] <= h:
                 res.pop()
