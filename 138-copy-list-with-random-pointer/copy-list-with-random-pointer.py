d8cd33b656618e4df870200062f5b8e3cbf97e93
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-01 17:52:50 -0700

    Time: 34 ms (80.48%) | Memory: 18.1 MB (5.57%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,6 +9,8 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # Node1 -> Node2 -> Node3
+        # Node1 -> Copy1 -> Node2 -> Copy2 -> Node3 -> Copy3
         if not head:
             return None
         
@@ -22,9 +24,10 @@ class Solution:
         newHead = head.next
         l1 = head
         while l1:
+            l2 = l1.next
             if l1.random:
                 l1.next.random = l1.random.next
-            l1 = l1.next.next
+            l1 = l2.next
         
         l1 = head
         while l1:
@@ -33,27 +36,4 @@ class Solution:
             if l2.next:
                 l2.next = l2.next.next
             l1 = l1.next
-        
         return newHead
-
-        
-       
-
-        
-
-
-        
-        
-        
-        
-        
-
-            
-
-                
-
-
-        
-       
-
-        
