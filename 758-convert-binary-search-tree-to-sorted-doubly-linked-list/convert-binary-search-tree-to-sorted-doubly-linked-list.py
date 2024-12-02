e6d207f6f940dace99f80be9841390ac27a3f580
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-01 21:19:03 -0700

    Time: 39 ms (44.07%) | Memory: 17.8 MB (5.60%) - LeetSync

---
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -0,0 +1,41 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        def dfs(node):
+            nonlocal first, last
+            if node:
+                dfs(node.left)
+
+                if not last:
+                    first = node
+                else:
+                    node.left = last
+                    last.right = node
+            
+                last = node
+
+                dfs(node.right)
+                
+        first, last = None, None
+
+        dfs(root)
+
+        first.left = last
+        last.right = first
+
+        return first
+
+
+            
+            
