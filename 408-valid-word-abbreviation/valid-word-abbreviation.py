0a4d2eb2cdc1297f82f71e604f940c37f28ee7db
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-02 23:38:22 -0700

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.20%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,7 +1,6 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i = j = 0
-
         while i < len(word) and j < len(abbr):
             if word[i] == abbr[j]:
                 i += 1
@@ -12,9 +11,9 @@ class Solution:
                 num_start = j
                 while j < len(abbr) and abbr[j].isdigit():
                     j += 1
-                num_string = abbr[num_start:j]
-                if num_string[0] == "0":
+                num_str = abbr[num_start:j]
+                if num_str[0] == "0":
                     return False
-                i += int(num_string)
-        
+                i += int(num_str)
+            
         return i == len(word) and j == len(abbr)
