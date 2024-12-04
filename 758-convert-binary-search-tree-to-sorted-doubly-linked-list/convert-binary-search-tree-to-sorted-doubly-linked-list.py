d9d088d4cdf6038aee47ec5a4a56bc188e3541c1
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-04 16:38:31 -0700

    Time: 36 ms (65.38%) | Memory: 18 MB (6.89%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -12,25 +12,24 @@ class Solution:
         if not root:
             return None
         self.first = self.last = None
+
         self.dfs(root)
 
         self.first.left = self.last
         self.last.right = self.first
         return self.first
-        
+    
     def dfs(self, node):
         if not node:
             return None
         
         self.dfs(node.left)
+
         if not self.first:
             self.first = node
         else:
             self.last.right = node
             node.left = self.last
-
+        
         self.last = node
         self.dfs(node.right)
-
-
-        
