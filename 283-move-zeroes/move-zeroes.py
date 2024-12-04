6c8dcada92f64641251362e52336a8a204411150
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-03 23:00:42 -0700

    Time: 1 ms (88.57%) | Memory: 18.3 MB (9.56%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,13 +5,9 @@ class Solution:
         """
         slow = 0
         for fast in range(len(nums)):
-            if nums[fast] != 0 and nums[slow] == 0:
+            if nums[fast] != 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
-            
-            if nums[slow] != 0:
                 slow += 1
-            
-
         return nums
 
-# 1 0 0 3 12
+# 1 3 12 0 0
