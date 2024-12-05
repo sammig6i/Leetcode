3c25734381bc9a04a9d39a4658dce1beb986d556
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-05 02:16:54 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (11.06%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,27 +1,33 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        num_list = list(str(num))
+        nums = list(str(num))
 
-        for i in range(len(num_list) - 1):
-            if num_list[i] < num_list[i + 1]:
+        for i in range(len(nums) - 1):
+            if nums[i] < nums[i + 1]:
+                max_idx, max_val = i + 1, nums[i + 1]
                 break
         else:
             return num
 
-        max_idx, max_val = i + 1, num_list[i + 1]
-        for j in range(i + 1, len(num_list)):
-            if max_val <= num_list[j]:
-                max_idx, max_val = j, num_list[j]
-
+        for j in range(i + 1, len(nums)):
+            if max_val <= nums[j]:
+                max_idx, max_val = j, nums[j]
+        
         left_idx = i
         for j in range(i, -1, -1):
-            if num_list[j] < max_val:
+            if nums[j] < max_val:
                 left_idx = j
-
-        num_list[max_idx], num_list[left_idx] = num_list[left_idx], num_list[max_idx]
-        return int(''.join(num_list))
+        
+        nums[left_idx], nums[max_idx] = nums[max_idx], nums[left_idx]
+        return int("".join(nums))
+        
+          
         
         # 2736 -> 7236
+        # max_idx = 1
+        # max_val = 7
+        # left_idx = 0
+
         # 98368 -> 98863
         # 120 -> 210
 
