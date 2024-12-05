51e15ef626b06ffb39d8b91638d067abf6d8e511
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-05 15:13:57 -0700

    Time: 11 ms (63.87%) | Memory: 19.7 MB (26.98%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -8,21 +8,33 @@ class Solution:
         if not lists:
             return None
         
+        return self.divide(lists, 0, len(lists) - 1)
+        
+    def divide(self, lists, L, R):
+        if L > R:
+            return None
+        if L == R:
+            return lists[L]
+        mid = (R + L) // 2
+        left = self.divide(lists, L, mid)
+        right = self.divide(lists, mid + 1, R)
+        return self.conquer(left, right)
+    
+    def conquer(self, l1, l2):
         head = ListNode()
         cur = head
-
-        while True:
-            min_node = -1
-
-            for i in range(len(lists)):
-                if not lists[i]:
-                    continue
-                if min_node == -1 or lists[min_node].val > lists[i].val:
-                    min_node = i
-
-            if min_node == -1:
-                break
-            cur.next = lists[min_node]
-            lists[min_node] = lists[min_node].next
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
             cur = cur.next
+        
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
         return head.next
+
