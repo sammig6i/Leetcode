cc9c68a41013b0be1952fe97bee1fc45c7110e51
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-04 21:35:58 -0700

    Time: 45 ms (73.86%) | Memory: 17.8 MB (8.87%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -3,14 +3,10 @@ class Solution:
         L = 0
         max_cons = 0
         for R, n in enumerate(nums):
-            if n == 0:
-                k -= 1
-        
+            k -= 1 - n
             if k < 0:
-                if nums[L] == 0:
-                    k += 1
+                k += 1 - nums[L]
                 L += 1
             else:
                 max_cons = max(max_cons, R - L + 1)
-
         return max_cons
