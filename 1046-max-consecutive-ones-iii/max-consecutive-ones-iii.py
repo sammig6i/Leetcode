56845a753df9278301caf882370dd4209140d51f
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-04 21:14:38 -0700

    Time: 55 ms (69.80%) | Memory: 17.8 MB (8.87%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,22 +1,12 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        if not nums:
-            return 0
-        max_ones = 0
         L = 0
-
+        max_cons = 0
         for R, n in enumerate(nums):
             k -= 1 - n
             if k < 0:
                 k += 1 - nums[L]
                 L += 1
             else:
-                max_ones = max(max_ones, R - L + 1)
-
-
-        
-           
-        return max_ones
-    #                    
-    # 1,1,1,0,0,0,1,1,1,1,0 k = 2
-    # 1,1,1,0,0,1,1,1,1,1,1
+                max_cons = max(max_cons, R - L + 1)
+        return max_cons
