1d36788c71616ab60ed4c54b4eaa6350cd03e24d
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-05 14:20:15 -0700

    Time: 1108 ms (12.64%) | Memory: 19.7 MB (34.80%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,32 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class NodeWrapper:
-    def __init__(self, node):
-        self.node = node
-    
-    def __lt__(self, other):
-        return self.node.val < other.node.val
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
         
-        min_heap = []
-        head = ListNode()
-        cur = head
+        for i in range(1, len(lists)):
+            lists[i] = self.mergeLists(lists[i - 1], lists[i])
+        return lists[-1]
 
-        for lst in lists:
-            if lst:
-                heapq.heappush(min_heap, NodeWrapper(lst))
+
         
-        while min_heap:
-            node_wrapper = heapq.heappop(min_heap)
-            cur.next = node_wrapper.node
-            cur = cur.next
-
-            if node_wrapper.node.next:
-                heapq.heappush(min_heap, NodeWrapper(node_wrapper.node.next))
-        return head.next
+    
+    def mergeLists(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        
+        return dummy.next
         
