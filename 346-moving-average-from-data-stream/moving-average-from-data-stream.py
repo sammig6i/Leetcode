fa6a36d455fab5ae3757bd829e824dfbc95456a6
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-04 21:58:26 -0700

    Time: 3 ms (82.59%) | Memory: 20.6 MB (12.15%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -2,18 +2,19 @@ class MovingAverage:
 
     def __init__(self, size: int):
         self.window = deque()
+        self.sum = 0
         self.size = size
 
     def next(self, val: int) -> float:
-        self.window.append(val)
+        if len(self.window) < self.size:
+            self.window.append(val)
+            self.sum += val
+        else:
+            self.sum -= self.window.popleft()
+            self.window.append(val)
+            self.sum += val
 
-        if len(self.window) > self.size:
-            self.window.popleft()
-
-        total_sum = 0
-        for n in self.window:
-            total_sum += n
-        return total_sum / len(self.window)
+        return self.sum / len(self.window)
 
 
         
