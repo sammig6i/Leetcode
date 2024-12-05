2902279887131de1713d7319acfd6bf332d565c3
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-05 16:22:24 -0700

    Time: 0 ms (100.00%) | Memory: 17.2 MB (11.47%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -5,6 +5,7 @@ class Solution:
         max_val = "0"
         max_i = -1
         swap_i = swap_j = -1
+
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] > max_val:
                 max_val = nums[i]
@@ -12,5 +13,5 @@ class Solution:
             if nums[i] < max_val:
                 swap_i = max_i
                 swap_j = i
-        nums[swap_j], nums[swap_i] = nums[swap_i], nums[swap_j]
+        nums[swap_i], nums[swap_j] = nums[swap_j], nums[swap_i]
         return int("".join(nums))
