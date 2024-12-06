360aa340d60e0f7d64c731f24213d18b732d70a4
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-05 23:05:51 -0700

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.87%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -6,26 +6,17 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d = defaultdict(list)
+        q = deque([(root, 0)])
+
         if not root:
             return []
-
-        d = defaultdict(list)
-        min_column = max_column = 0
-
-        def dfs(node, row, col):
-            if node:
-                nonlocal min_column, max_column
-                d[col].append((row, node.val))
-                min_column = min(min_column, col)
-                max_column = max(max_column, col)
-
-                dfs(node.left, row + 1, col - 1)
-                dfs(node.right, row + 1, col + 1)
         
-        dfs(root, 0, 0)
-        res = []
-        for col in range(min_column, max_column + 1):
-            d[col].sort(key=lambda x: x[0])
-            colVals = [v for _, v in d[col]]
-            res.append(colVals)
-        return res
+        while q:
+            node, col = q.popleft()
+            if node:
+                d[col].append(node.val)
+
+                q.append((node.left, col - 1))
+                q.append((node.right, col + 1))
+        return [v for _, v in sorted(d.items())]
