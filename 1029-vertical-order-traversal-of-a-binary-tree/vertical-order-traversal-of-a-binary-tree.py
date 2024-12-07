c2add50876ba7b97f822c1d10afc08044a31f79b
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-07 03:44:32 -0700

    Time: 3 ms (24.43%) | Memory: 17.8 MB (7.20%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -9,32 +9,27 @@ class Solution:
         if not root:
             return None
         
-        self.d = defaultdict(list)
-        self.min_col = self.max_col = 0
-        self.dfs(root, 0, 0)
+        q = deque([(root, 0, 0)])
+        d = defaultdict(list)
+
+        min_col = max_col = 0
+
+        while q:
+            node, row, col = q.popleft()
+            if node:
+                d[col].append((node.val, row))
+                
+                min_col = min(min_col, col)
+                max_col = max(max_col, col)
+
+                q.append((node.left, row + 1, col - 1))
+                q.append((node.right, row + 1, col + 1))
         
         res = []
-        for c in range(self.min_col, self.max_col + 1):
-            self.d[c].sort(key=lambda x: (x[0], x[1]))
-
-            vals = [v for _, v in self.d[c]]
+        for col in range(min_col, max_col + 1):
+            d[col].sort(key=lambda x: (x[1], x[0]))
+            vals = [v for v, _ in d[col]]
             res.append(vals)
-
         return res
 
-    def dfs(self, node, r, c):
-        if not node:
-            return None
-        
-        self.d[c].append((r, node.val))
-        self.min_col = min(self.min_col, c)
-        self.max_col = max(self.max_col, c)
-
-        self.dfs(node.left, r + 1,  c - 1)
-        self.dfs(node.right, r + 1, c + 1)
-
-# -2: (0, 2)
-# -1: (1, 1)
-# 0: (3, 0), (2, 2), (2, 2)
-# 1: (4, 1)
         
