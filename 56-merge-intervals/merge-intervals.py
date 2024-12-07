ec87f9b5f6be468e22eba9a6ed6db46fb2fab4bd
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-07 14:56:16 -0700

    Time: 7 ms (74.56%) | Memory: 21.9 MB (8.16%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,14 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        max_val = max(interval[0] for interval in intervals)
+        max_start = max(interval[0] for interval in intervals)
 
-        mp = [0] * (max_val + 1)
+        mp = [0] * (max_start + 1)
+
         for start, end in intervals:
             mp[start] = max(end + 1, mp[start])
         
         res = []
         have = -1
         interval_start = -1
+
         for i in range(len(mp)):
             if mp[i] != 0:
                 if interval_start == -1:
@@ -23,9 +25,3 @@ class Solution:
             res.append([interval_start, have])
         
         return res
-
-
-# 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-# 0, 4, 7, 0, 0, 0, 0, 0, 11, 0, 0,  0,  0,  0,  0, 19 
-# interval_start = 15
-# have = 18
