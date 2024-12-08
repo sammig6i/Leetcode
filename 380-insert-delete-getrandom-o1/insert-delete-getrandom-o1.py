395dcbd96dbde0727b0bd959b59542fde5ff989d
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-08 02:34:38 -0700

    Time: 106 ms (77.58%) | Memory: 57.7 MB (17.67%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -16,8 +16,8 @@ class RandomizedSet:
             idx = self.indices[val]
             last_val = self.list[-1]
             self.list[idx], self.list[-1] = self.list[-1], self.list[idx]
-            self.indices[last_val] = idx
             self.list.pop()
+            self.indices[last_val] = idx
             del self.indices[val]
             return True
         return False
