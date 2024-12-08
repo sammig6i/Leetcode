05214404b33627f1a4ae016dc18b4fadf0694f51
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-08 16:06:48 -0700

    Time: 126 ms (49.00%) | Memory: 73.7 MB (8.00%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -12,6 +12,7 @@ class RandomizedCollection:
         
         self.indices[val] = {len(self.list) - 1}
         return True
+        
 
     def remove(self, val: int) -> bool:
         if val not in self.indices:
@@ -19,12 +20,12 @@ class RandomizedCollection:
         
         idx = self.indices[val].pop()
         last_val = self.list[-1]
-
         self.list[idx], self.list[-1] = self.list[-1], self.list[idx]
+
         self.indices[last_val].add(idx)
         self.indices[last_val].remove(len(self.list) - 1)
-
         self.list.pop()
+
         if not self.indices[val]:
             del self.indices[val]
         return True
@@ -32,6 +33,7 @@ class RandomizedCollection:
 
     def getRandom(self) -> int:
         return random.choice(self.list)
+        
 
 
 # Your RandomizedCollection object will be instantiated and called as such:
