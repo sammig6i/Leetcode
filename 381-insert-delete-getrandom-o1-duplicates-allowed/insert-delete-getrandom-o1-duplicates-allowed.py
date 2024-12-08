4bbcf6c8e9908b233993609e65a5d519f1be2b00
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-08 02:53:34 -0700

    Time: 123 ms (53.93%) | Memory: 72.9 MB (62.47%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -3,15 +3,15 @@ class RandomizedCollection:
     def __init__(self):
         self.list = []
         self.indices = {}
-
+        
     def insert(self, val: int) -> bool:
         self.list.append(val)
         if val in self.indices:
             self.indices[val].add(len(self.list) - 1)
             return False
-        else:
-            self.indices[val] = {len(self.list) - 1}
-            return True
+        
+        self.indices[val] = {len(self.list) - 1}
+        return True
 
     def remove(self, val: int) -> bool:
         if val not in self.indices:
@@ -19,21 +19,19 @@ class RandomizedCollection:
 
         idx = self.indices[val].pop()
         last_val = self.list[-1]
-
+        
         if idx != len(self.list) - 1:
-            self.list[idx] = last_val
-            last_val = self.list[idx]
+            self.list[idx], self.list[-1] = self.list[-1], self.list[idx]
             self.indices[last_val].discard(len(self.list) - 1)
             self.indices[last_val].add(idx)
-        
+    
         self.list.pop()
         if not self.indices[val]:
             del self.indices[val]
         return True
-# [2, 1]
-# {1: 0, 2: 1}
-# idx = 0
-# last_val = 1
+# [1, 3, 3]
+# 1: 0,, 3: [2, 1]
+
     def getRandom(self) -> int:
         return random.choice(self.list)
         
