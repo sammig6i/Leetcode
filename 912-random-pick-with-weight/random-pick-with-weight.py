4e890a3dac781df4acc5dac1a8c7d9cc961f061e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-09 02:21:50 -0700

    Time: 74 ms (55.24%) | Memory: 22.4 MB (12.14%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -15,13 +15,16 @@ class Solution:
         while L < R:
             m = (R + L) // 2
 
-            if self.pre[m] < target:
-                L = m + 1
-            elif self.pre[m] > target:
+            if self.pre[m] >= target:
                 R = m
+            else:
+                L = m + 1
         return L
 
-# [1,4]
+# [1, 2, 5]
+
+# [1, 3, 8]
+
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
