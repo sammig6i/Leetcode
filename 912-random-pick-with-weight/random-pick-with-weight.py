5e23ad495a74453d9d04d4091b24b4e0acd556bc
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-09 02:15:10 -0700

    Time: 80 ms (45.40%) | Memory: 22.5 MB (7.91%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,29 +1,29 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.s = []
+        self.pre = []
         total = 0
         for weight in w:
             total += weight
-            self.s.append(total)
+            self.pre.append(total)
+        
         self.total = total
 
-
     def pickIndex(self) -> int:
+        L, R = 0, len(self.pre) - 1
         target = random.uniform(0, self.total)
-        L, R = 0, len(self.s) - 1
-
         while L < R:
-            m = (L + R) // 2
+            m = (R + L) // 2
 
-            if self.s[m] <= target:
+            if self.pre[m] < target:
                 L = m + 1
-            else:
+            elif self.pre[m] > target:
                 R = m
+            else:
+                return L
         return L
-        
 
-
+# [1,4]
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
