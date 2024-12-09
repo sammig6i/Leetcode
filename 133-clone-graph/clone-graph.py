439561a8af5308485cfa9fa5300cba6b411cfc34
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-09 00:21:13 -0700

    Time: 43 ms (38.49%) | Memory: 17.9 MB (7.38%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -16,8 +16,6 @@ class Solution:
         return self.oldToNew[node]
 
     def dfs(self, node):
-        # if not node:
-            # return None
         if node in self.oldToNew:
             return self.oldToNew[node]
         
