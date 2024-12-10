fbdb5fc1ffcb872e32c90f2bff6f693773fa5754
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-09 22:44:49 -0700

    Time: 1 ms (49.99%) | Memory: 17.4 MB (5.89%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,10 +1,27 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        
+        def is_diagonal(row, col):
+            val = matrix[row][col]
+
+            while row < len(matrix) and col < len(matrix[0]):
+                if matrix[row][col] != val:
+                    return False
+                
+                row += 1
+                col += 1
+            return True
+        
         rows = len(matrix) - 1
         cols = len(matrix[0]) - 1
 
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] != matrix[r + 1][c + 1]:
-                    return False
+        for row in range(rows):
+            if not is_diagonal(row, 0):
+                return False
+        
+        for col in range(cols):
+            if not is_diagonal(0, col):
+                return False
+        
         return True
+
