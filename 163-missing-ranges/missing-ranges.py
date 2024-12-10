5efb146db28a114e857ea40b024b638177817b45
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-09 18:45:31 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (6.59%) - LeetSync

--- 163-missing-ranges/missing-ranges.py
+++ 163-missing-ranges/missing-ranges.py
@@ -1,25 +1,23 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
-        n = len(nums)
-        missing_ranges = []
-
-        if n == 0:
+        if not len(nums):
             return [[lower, upper]]
+        
+        res = []
 
-        if lower < nums[0]:
-            missing_ranges.append([lower, nums[0] - 1])
+        if nums[0] > lower:
+            res.append([lower, nums[0] - 1])
 
-        for i in range(n - 1):
-            if nums[i + 1] - nums[i] <= 1:
+        for i in range(len(nums)):
+            if nums[i] - nums[i - 1] <= 1:
                 continue
+            
+            res.append([nums[i - 1] + 1, nums[i] - 1])
 
-            missing_ranges.append([nums[i] + 1, nums[i + 1] - 1])
-
-        if upper > nums[n - 1]:
-            missing_ranges.append([nums[n - 1] + 1, upper])
-
-        return missing_ranges  
-
-[0, 1, 3, 50, 75], 100
-# 0, 0, 2 [2, 2], [4, 49], [51, 74], [76 - 99]
-# nums - sorted unique integers, all elements within range
+        if nums[len(nums) - 1] < upper:
+            res.append([nums[len(nums) - 1] + 1, upper])
+        
+        return res
+            
+            
+            
