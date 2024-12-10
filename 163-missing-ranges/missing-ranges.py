0732773c415186f1207163309378f280f20d001d
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-09 18:48:20 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.59%) - LeetSync

--- 163-missing-ranges/missing-ranges.py
+++ 163-missing-ranges/missing-ranges.py
@@ -8,11 +8,11 @@ class Solution:
         if nums[0] > lower:
             res.append([lower, nums[0] - 1])
 
-        for i in range(len(nums) - 1):
-            if nums[i + 1] - nums[i] <= 1:
+        for i in range(len(nums)):
+            if nums[i] - nums[i - 1] <= 1:
                 continue
             
-            res.append([nums[i] + 1, nums[i + 1] - 1])
+            res.append([nums[i - 1] + 1, nums[i] - 1])
 
         if nums[len(nums) - 1] < upper:
             res.append([nums[len(nums) - 1] + 1, upper])
