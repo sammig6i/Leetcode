a51a3e610321e9130d0107fd4e81f753ef541332
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-10 23:32:24 -0700

    Time: 42 ms (29.68%) | Memory: 18.2 MB (6.53%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
@@ -9,28 +9,22 @@ class Node:
 class Solution:
     def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
         node = Node(insertVal)
-
         if not head:
             head = node
             head.next = head
-            return head
         
+
         prev, cur = head, head.next
-
         while cur != head:
-            # 1 -> Node(2) -> 3
             if prev.val <= node.val <= cur.val:
-               break
-
-            # 4 -> Node(5) -> 1, 4 -> Node(0) -> 1
-            elif prev.val > cur.val and (node.val > prev.val or node.val < cur.val):
                 break
-
+            
+            if prev.val > cur.val and (node.val > prev.val or node.val < cur.val):
+                break
+            
             prev, cur = cur, cur.next
         
-
-        node.next = cur
         prev.next = node
+        node.next = cur
 
         return head
-        # 3, 4, 0, 1
