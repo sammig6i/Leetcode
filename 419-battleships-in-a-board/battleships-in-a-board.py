a64cc0e391e5c554f0a5f188f2914866f26c59e9
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-11 00:55:02 -0700

    Time: 0 ms (100.00%) | Memory: 19.7 MB (21.04%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -3,26 +3,14 @@ class Solution:
         if not board:
             return 0
 
-        def dfs(r, c):
-            board[r][c] = "."
-
-            for d in directions:
-                new_r = r + d[0]
-                new_c = c + d[1]
-            
-                if 0 <= new_r < M and 0 <= new_c < N and board[new_r][new_c] == "X":
-                    dfs(new_r, new_c)
-
         M = len(board)
         N = len(board[0])
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         res = 0
         for r in range(M):
             for c in range(N):
                 if board[r][c] == "X":
-                    res += 1
-                    dfs(r, c)
-        
+                    if ((r == 0 or board[r - 1][c] == ".") and
+                    (c == 0 or board[r][c - 1] == ".")):
+                        res += 1
         return res
         
-    
