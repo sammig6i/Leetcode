447e22934494146deb033b3ca895922806344aa2
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-11 13:12:20 -0700

    Time: 2 ms (56.61%) | Memory: 19.2 MB (6.93%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -3,11 +3,12 @@ class Solution:
         arr = []
         for i, n in enumerate(nums):
             arr.append([n, i])
-        
         arr.sort()
         L, R = 0, len(nums) - 1
+
         while L < R:
             cur = arr[L][0] + arr[R][0]
+
             if cur == target:
                 return [min(arr[L][1], arr[R][1]),
                         max(arr[L][1], arr[R][1])]
