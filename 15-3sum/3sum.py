070ae7741472828f5673ccab94e12a7a2babae36
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-11 02:06:23 -0700

    Time: 2863 ms (5.01%) | Memory: 60.8 MB (5.41%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
+        res = []
         count = defaultdict(int)
         for n in nums:
             count[n] += 1
-
-        res = []
+        
         for i in range(len(nums)):
             count[nums[i]] -= 1
             if i and nums[i] == nums[i - 1]:
@@ -15,11 +15,13 @@ class Solution:
                 count[nums[j]] -= 1
                 if j - 1 > i and nums[j] == nums[j - 1]:
                     continue
+                
                 target = -(nums[i] + nums[j])
                 if count[target] > 0:
-                    res.append([nums[i], nums[j], target])
+                    res.append([target, nums[i], nums[j]])
             
             for j in range(i + 1, len(nums)):
                 count[nums[j]] += 1
         
         return res
+            
