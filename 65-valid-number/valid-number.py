62c2a6e74fbda3ecaf720e6390031b57d52a8f8e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-10 22:43:15 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.24%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        # integer followed by exponent
-        # decimal number followed by exponent
-        # - or + followed by integers or decimals
-        # digits followed by a dot
-        # digits followed by a dot followed by digits
-        # a dot followed by digits
-        # integers after an exponent 'e' or 'E'
-
+        if not s:
+            return False
         num_seen = False
         decimal_seen = False
 
@@ -17,13 +11,14 @@ class Solution:
         
         while i < len(s):
             c = s[i]
+
+            if c.isdigit():
+                num_seen = True
             if c.isalpha():
                 if c in ("e", "E"):
                     return num_seen and self.is_valid_integer(s[i + 1:])
                 else:
                     return False
-            if c.isdigit():
-                num_seen = True
             if c in ("+", "-"):
                 return False
             if c == ".":
@@ -33,24 +28,28 @@ class Solution:
                     decimal_seen = True
             
             i += 1
-
         return num_seen
-
+    
     def is_valid_integer(self, s):
-        if not len(s):
+        if not s:
             return False
         
         num_seen = False
+
         i = 0
         if s[i] in ("+", "-"):
             i += 1
-
+        
         while i < len(s):
             c = s[i]
-            if not c.isdigit():
-                return False
-            else:
+            if c.isdigit():
                 num_seen = True
+            if c.isalpha():
+                return False
+            if c in ("+", "-"):
+                return False
+            if c == ".":
+                return False
             i += 1
         
         return num_seen
