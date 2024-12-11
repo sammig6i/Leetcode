cddbe1df3090e72b056a7940587f9ad4354a747e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-11 14:28:29 -0700

    Time: 0 ms (100.00%) | Memory: 17.3 MB (7.16%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -7,18 +7,16 @@ class Solution:
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
                 pivot = i
-                # nums[i], nums[i + 1] = nums[i + 1], nums[i]
                 break
-
+        
         if pivot == -1:
-            nums.sort()
+            nums.reverse()
             return nums
-
-        # 132 -> 213
-        # 231
-        for i in range(len(nums) - 1, pivot, -1):
+        
+        for i in range(len(nums) - 1, -1, -1):
             if nums[i] > nums[pivot]:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
                 break
         nums[pivot + 1:] = reversed(nums[pivot + 1:])
         return nums
+
