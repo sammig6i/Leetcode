aa7c7b9151acb1c63450b8b86f48df64025e5294
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-14 01:54:56 -0700

    Time: 4 ms (49.92%) | Memory: 18.6 MB (5.63%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i, n in enumerate(nums):
-            if n - i > 0:
+        num_set = set(nums)
+        n = len(nums)
+        for i in range(n + 1):
+            if i not in num_set:
                 return i
-        return len(nums)
     
     #  [9,6,4,2,3,5,7,0,1]
