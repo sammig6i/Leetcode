8e4ef5b011bc51d5fbdeff6ca0416b8873b06a7a
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-15 00:07:39 -0700

    Time: 10 ms (70.69%) | Memory: 22.5 MB (51.82%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        if len(original) != m * n:
+        if len(original) != m*n:
             return []
         
         res = []
