3d46df797fe938e9207216ed7847eab2d9c1a914
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-16 14:55:01 -0700

    Time: 14 ms (95.92%) | Memory: 23.2 MB (60.60%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -6,11 +6,5 @@ class Solution:
             idx = n - 1
             if nums[idx] < 0:
                 res.append(n)
-            else:
-                nums[idx] = -(nums[idx])
-
+            nums[idx] = -nums[idx]
         return res
-        
-[4,3,2,7,8,2,3,1]
-
-[-1,-1,2]
