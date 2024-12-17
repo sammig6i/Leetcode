5dc27b426832ac85e31d76caa3bbff31bbcf6c67
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-17 00:28:39 -0700

    Time: 7 ms (23.09%) | Memory: 18.6 MB (5.49%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,26 +4,16 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         rows, cols = len(matrix), len(matrix[0])
-        row_zero = False
+        mark = [[matrix[r][c] for c in range(cols)] for r in range(rows)]
 
         for r in range(rows):
             for c in range(cols):
                 if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    if r > 0:
-                        matrix[r][0] = 0
-                    else:
-                        row_zero = True
+                    for col in range(cols):
+                        mark[r][col] = 0
+                    for row in range(rows):
+                        mark[row][c] = 0
 
-        for r in range(1, rows):
-            for c in range(1, cols):
-                if matrix[0][c] == 0 or matrix[r][0] == 0:
-                    matrix[r][c] = 0
-
-        if matrix[0][0] == 0:
-            for r in range(rows):
-                matrix[r][0] = 0
-        
-        if row_zero:
+        for r in range(rows):
             for c in range(cols):
-                matrix[0][c] = 0
+                matrix[r][c] = mark[r][c]
