1c7f5c8574477c2670561657beb80e7158ecb33c
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-17 12:20:20 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.41%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -15,7 +15,7 @@ class Solution:
 
             if not (left < right and top < bottom):
                 break
-
+            
             for i in range(right - 1, left - 1, -1):
                 res.append(matrix[bottom - 1][i])
             bottom -= 1
@@ -23,8 +23,5 @@ class Solution:
             for i in range(bottom - 1, top - 1, -1):
                 res.append(matrix[i][left])
             left += 1
-
         return res
-
-
-# [1,2,3,6,9,8,7,4]
+            
