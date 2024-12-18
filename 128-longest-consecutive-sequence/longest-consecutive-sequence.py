7ce5dc121e6e452b43e893f8c91401ce7cf2d1ff
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-18 01:59:14 -0700

    Time: 189 ms (39.74%) | Memory: 40.8 MB (6.94%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -3,10 +3,10 @@ class Solution:
         d = defaultdict(int)
         res = 0
 
-        for n in nums:
-            if not d[n]:
-                d[n] = d[n - 1] + d[n + 1] + 1
-                d[n - d[n - 1]] = d[n]
-                d[n + d[n + 1]] = d[n]
-                res = max(res, d[n])
+        for num in nums:
+            if not d[num]:
+                d[num] = d[num - 1] + d[num + 1] + 1
+                d[num - d[num - 1]] = d[num]
+                d[num + d[num + 1]] = d[num]
+                res = max(res, d[num])
         return res
