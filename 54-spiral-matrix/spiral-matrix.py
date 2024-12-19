2de77a3f2479ff60a8a11f03185427f469173b14
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-19 04:06:05 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.19%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -7,12 +7,12 @@ class Solution:
             if row == 0 or col == 0:
                 return
             
-            for i in range(col):
+            for _ in range(col):
                 r += dr
                 c += dc
                 res.append(matrix[r][c])
             
             dfs(col, row - 1, r, c, dc, -dr)
-
+        
         dfs(m, n, 0, -1, 0, 1)
         return res
