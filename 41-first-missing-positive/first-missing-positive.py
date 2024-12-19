971e36164e94180f05fff00c8ab59c377d3226b1
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-19 15:49:48 -0700

    Time: 28 ms (76.69%) | Memory: 29.4 MB (26.37%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -4,8 +4,9 @@ class Solution:
         for n in nums:
             if n > 0 and n <= len(nums):
                 seen[n - 1] = True
-            
+
         for n in range(1, len(nums) + 1):
-            if not seen[n - 1]:
+            idx = n - 1
+            if not seen[idx]:
                 return n
         return len(nums) + 1
