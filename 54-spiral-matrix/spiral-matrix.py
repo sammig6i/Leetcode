3a8a2eadf9b1ff8cae1ac64b20d68c01f0a623d1
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-19 01:29:53 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.19%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,19 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        m, n = len(matrix), len(matrix[0])
         res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
 
-        def dfs(row, col, r, c, dr, dc):
-            if row == 0 or col == 0:
-                return
-            
-            for i in range(col):
-                r += dr
-                c += dc
-                res.append(matrix[r][c])
-            
-            dfs(col, row - 1, r, c, dc, -dr)
-            return res
-        
-        return dfs(m, n, 0, -1, 0, 1)
+        while left < right and top < bottom:
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            right -= 1
 
+            for i in range(top + 1, bottom):
+                res.append(matrix[i][right])
+            bottom -= 1
+            
+            if not (left < right and top < bottom):
+                break
+            
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom][i])
+            top += 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
+        return res
+
+
+            
+            
+            
