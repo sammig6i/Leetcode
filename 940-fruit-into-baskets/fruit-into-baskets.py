732b8668b15aa109f05e1a1d910a14135a303e10
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-20 18:35:46 -0700

    Time: 79 ms (97.22%) | Memory: 23.7 MB (11.11%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -10,19 +10,29 @@ class Solution:
         for R in range(len(fruits)):
             f = fruits[R]
             if f == fruit1:
-                total += 1
+                # total += 1
                 fruit1_lastIdx = R
             elif f == fruit2 or fruit2 == -1:
-                total += 1
+                # total += 1
                 fruit2_lastIdx = R
                 fruit2 = f
             else:
                 if fruit2_lastIdx == min(fruit1_lastIdx, fruit2_lastIdx):
                     fruit1_lastIdx, fruit2_lastIdx = fruit2_lastIdx, fruit1_lastIdx
                     fruit1, fruit2 = fruit2, fruit1
-                total -= (fruit1_lastIdx - L + 1)
+                # total -= (fruit1_lastIdx - L + 1)
                 L = fruit1_lastIdx + 1
                 fruit1 = f
                 fruit1_lastIdx = R
             res = max(res, R - L + 1)
         return res
+# 1,2,1,2,2,3
+fruit1 = 3
+fruit2 = 2
+fruit1_lastIdx = 5
+fruit2_lastIdx = 4
+
+f = 3
+total = 2
+res = 5
+L = 3
