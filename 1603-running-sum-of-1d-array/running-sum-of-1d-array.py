3733e640bd11e89ef0dc760490160c7d89ccef2e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-21 22:56:38 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.22%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -2,6 +2,6 @@ class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         total = 0
         prefix = [nums[0]]
-        for n in nums[1:]:
-            prefix.append(n + prefix[-1])
+        for i in range(1, len(nums)):
+            prefix.append(nums[i] + prefix[-1])
         return prefix
