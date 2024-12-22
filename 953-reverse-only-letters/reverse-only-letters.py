d2e2e0a60c2285453c82a294436709056ef28d32
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-22 15:37:53 -0700

    Time: 1 ms (13.56%) | Memory: 17.7 MB (5.80%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.py
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -1,16 +1,4 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        s = list(s)
-        L, R = 0, len(s) - 1
-        while L < R:
-            if not s[L].isalpha():
-                L += 1
-            elif not s[R].isalpha():
-                R -= 1
-            else:
-                s[L], s[R] = s[R], s[L]
-                L += 1
-                R -= 1
-        return "".join(s)
-        
-# "Test1ng-Leet=code-Q!
+        chars = [c for c in s if c.isalpha()]
+        return "".join(c if not c.isalpha() else chars.pop() for c in s)
