2fce375047d87e18e22032b28ae267d195fd0024
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-22 15:58:36 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (6.09%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -3,7 +3,12 @@ class Solution:
         word_list = list(word)
         for i in range(len(word_list)):
             if word_list[i] == ch:
-                reversed_portion = word_list[:i + 1][::-1]
-                word_list[:i + 1] = reversed_portion
+                L, R = 0, i
+                while L < R:
+                    word_list[L], word_list[R] = word_list[R], word_list[L]
+                    L += 1
+                    R -= 1
+                # reversed_portion = word_list[:i + 1][::-1]
+                # word_list[:i + 1] = reversed_portion
                 break
         return "".join(word_list)
