7ad3506aa7714d0d36c295c29e87a66b3583749c
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-22 16:20:33 -0700

    Time: 111 ms (29.08%) | Memory: 17.9 MB (15.78%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -6,10 +6,10 @@ class Solution:
         res = float("-inf")
         for R in range(len(s)):
             vowel_count += 1 if s[R] in vowels else 0
-            if R - L + 1 == k:
-                res = max(res, vowel_count)
+            if R - L + 1 > k:
                 vowel_count -= 1 if s[L] in vowels else 0
                 L += 1
+            res = max(res, vowel_count)
         return res
     
 # abciiidef
