c71726d9a4c84477443515ca7060a807a73cd30e
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-22 16:38:07 -0700

    Time: 95 ms (45.94%) | Memory: 21.6 MB (5.25%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         n = len(s)
-        vowels = {'a', 'e', 'i', 'o', 'u'}
         res = 0
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i + 1] = prefix[i] + (1 if s[i] in vowels else 0)
@@ -10,4 +11,3 @@ class Solution:
         for i in range(k, n + 1):
             res = max(res, prefix[i] - prefix[i - k])
         return res
-# prefix = [0,1,1,1,2,3,4,4,5,5]
