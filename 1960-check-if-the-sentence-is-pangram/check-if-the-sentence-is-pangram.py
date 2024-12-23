abd873408b86ac3b521d73fc7b46c73118cd41c7
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-23 15:15:07 -0700

    Time: 2 ms (21.70%) | Memory: 17.6 MB (6.47%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,4 +1,11 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        char_set = set(sentence)
-        return len(char_set) == 26
+        seen = 0
+
+        for c in sentence:
+            curr_char = ord(c) - ord("a")
+
+            masked_bit = 1 << curr_char
+
+            seen |= masked_bit
+        return seen == (1 << 26) - 1
