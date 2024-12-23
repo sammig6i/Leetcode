0c9fe9e70f316366b53c29fbe56c321575387ec8
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-23 13:27:50 -0700

    Time: 92 ms (63.77%) | Memory: 34.9 MB (9.36%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -1,8 +1,8 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
-        res = [-1] * n
         window_size = 2 * k + 1
+        res = [-1] * n
 
         if window_size > n:
             return res
@@ -15,5 +15,3 @@ class Solution:
             curr_sum = prefix[i + k + 1] - prefix[i - k]
             res[i] = curr_sum // window_size
         return res
-
-
