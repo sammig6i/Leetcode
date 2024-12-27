a574e0347e786d48a3e978df0b8b192649735b57
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-26 20:19:44 -0700

    Time: 4 ms (23.14%) | Memory: 18 MB (5.84%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         balloon = Counter("balloon")
-        word = Counter(text)
+        word_count = Counter(text)
 
         res = len(text)
-
         for c in balloon:
-            res = min(res, word[c] // balloon[c])
-        return res
-
+            res = min(res, word_count[c] // balloon[c])
         
-
+        return res
