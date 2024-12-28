e28d622753feeb93808ec70d2f45a259cfe04d30
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-28 13:54:12 -0700

    Time: 143 ms (37.28%) | Memory: 59.5 MB (65.29%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -4,16 +4,15 @@ class Solution:
         losses_count = defaultdict(int)
 
         for winner, loser in matches:
-            seen.add(winner)
-            seen.add(loser)
+            losses_count[winner] = losses_count.get(winner, 0)
             losses_count[loser] += 1
         
         one_loss = []
         zero_loss = []
-        for player in seen:
-            if player not in losses_count:
+        for player, losses in losses_count.items():
+            if losses == 0:
                 zero_loss.append(player)
-            elif losses_count[player] == 1:
+            elif losses == 1:
                 one_loss.append(player)
         return [sorted(zero_loss), sorted(one_loss)]
 
