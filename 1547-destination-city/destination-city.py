0b8faa05d6575831e2ccb2a62c3f034f7f711766
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-28 19:47:42 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (13.23%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,9 +1,8 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        mp = {}
-        for start, dest in paths:
-            mp[start] = dest
-        
-        for start, dest in paths:
-            if dest not in mp:
-                return dest
+        mp = {p[0]: p[1] for p in paths}
+
+        start = paths[0][0]
+        while start in mp:
+            start = mp[start]
+        return start
