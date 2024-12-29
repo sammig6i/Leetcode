b66a4cfb0643d06c8a8e060638b5cb55ec6d2eab
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-28 18:46:32 -0700

    Time: 23 ms (49.67%) | Memory: 17.8 MB (13.53%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,3 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return Counter(magazine) >= Counter(ransomNote)
+        mp = defaultdict(int)
+        for c in magazine:
+            mp[c] += 1
+        
+        for c in ransomNote:
+            if c not in mp:
+                return False
+            mp[c] -= 1
+            if mp[c] == 0:
+                del mp[c]
+        return True
