31c329c0684e034cb11342db01d59454948cd67b
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-30 02:07:43 -0700

    Time: 32 ms (47.29%) | Memory: 18.7 MB (55.66%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -4,13 +4,16 @@ class Solution:
             if x < 0:
                 return 0
             
+            L = 0
+            cur = 0
             res = 0
-            L, cur = 0, 0
             for R in range(len(nums)):
                 cur += nums[R]
+
                 while cur > x:
                     cur -= nums[L]
                     L += 1
+                
                 res += (R - L + 1)
             return res
         
