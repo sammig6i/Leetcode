74df929d8a9416ec881d6664a5c2d64898f97ee6
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-30 21:48:49 -0700

    Time: 119 ms (23.95%) | Memory: 23.5 MB (17.30%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,18 +1,23 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         diff_idx = {}
-        count = 0
+        ones = 0
+        zeros = 0
         res = 0
 
         for i, n in enumerate(nums):
-            count += 1 if n == 1 else -1
-            
-            if count not in diff_idx:
-                diff_idx[count] = i
-            
-            if count == 0:
-                res = i + 1
+            if n == 1:
+                ones += 1
             else:
-                idx = diff_idx[count]
+                zeros += 1
+            
+            diff = ones - zeros
+            if diff not in diff_idx:
+                diff_idx[diff] = i
+            
+            if diff == 0:
+                res = ones + zeros
+            else:
+                idx = diff_idx[diff]
                 res = max(res, i - idx)
         return res
