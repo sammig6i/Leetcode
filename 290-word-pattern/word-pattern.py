ddaecef6876be7eeb723c922d9dd15885c53d31c
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-31 01:02:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (11.87%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,15 +1,19 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        charToWord = defaultdict(int)
-        wordToChar = defaultdict(int)
         words = s.split()
-
         if len(pattern) != len(words):
             return False
         
+        charToWord = {}
+        store = set()
+
         for i, (c, w) in enumerate(zip(pattern, words)):
-            if charToWord[c] != wordToChar[w]:
-                return False
-            charToWord[c] = i + 1
-            wordToChar[w] = i + 1
+            if c in charToWord:
+                if words[charToWord[c]] != w:
+                    return False
+            else:
+                if w in store:
+                    return False
+                charToWord[c] = i
+                store.add(w)
         return True
