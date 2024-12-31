c53876d0194a451d4a22f6082c144b09eba2d2e6
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-12-31 00:24:57 -0700

    Time: 12 ms (9.91%) | Memory: 17.9 MB (20.16%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,9 +5,8 @@ class Solution:
         
         mapST, mapTS = {}, {}
         for i in range(len(s)):
-            if s[i] in mapST and mapST[s[i]] != t[i]:
-                return False
-            if t[i] in mapTS and mapTS[t[i]] != s[i]:
+            if (s[i] in mapST and mapST[s[i]] != t[i] or
+                t[i] in mapTS and mapTS[t[i]] != s[i]):
                 return False
             mapST[s[i]] = t[i]
             mapTS[t[i]] = s[i]
