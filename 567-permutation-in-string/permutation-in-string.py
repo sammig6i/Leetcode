508dab27c4b10e274fa9447f769b5daf39e10994
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-04 01:02:52 -0700

    Time: 27 ms (48.78%) | Memory: 17.8 MB (24.02%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,32 +3,18 @@ class Solution:
         if len(s1) > len(s2):
             return False
         
-        s1_count, s2_count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1_count[ord(s1[i]) - ord('a')] += 1
-            s2_count[ord(s2[i]) - ord('a')] += 1
-        
-        matches = 0
-        for i in range(26):
-            matches += (1 if s1_count[i] == s2_count[i] else 0) 
-        
+        s1_count = Counter(s1)
         L = 0
-        for R in range(len(s1), len(s2)):
-            if matches == 26:
+        s2_count = defaultdict(int)
+        for R in range(len(s2)):
+            s2_count[s2[R]] += 1
+
+            while (R - L + 1) > len(s1):
+                s2_count[s2[L]] -= 1
+                if not s2_count[s2[L]]:
+                    del s2_count[s2[L]]
+                L += 1
+            
+            if s1_count == s2_count:
                 return True
-            
-            idx = ord(s2[R]) - ord('a')
-            s2_count[idx] += 1
-            if s1_count[idx] == s2_count[idx]:
-                matches += 1
-            elif s1_count[idx] + 1 == s2_count[idx]:
-                matches -= 1
-            
-            idx = ord(s2[L]) - ord('a')
-            s2_count[idx] -= 1
-            if s1_count[idx] == s2_count[idx]:
-                matches += 1
-            elif s1_count[idx] - 1 == s2_count[idx]:
-                matches -= 1
-            L += 1
-        return matches == 26
+        return False
