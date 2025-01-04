af790ce6fb8af8697b9a498525488ff399049fab
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-04 00:26:11 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.07%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -5,33 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
-        prev = dummy
-        for _ in range(left - 1):
-            prev = prev.next
-
-        sublist_head = prev.next
-        sublist_tail = sublist_head
-        for _ in range(right - left):
-            sublist_tail = sublist_tail.next
+        def reverseList(node, n):
+            if n == 1:
+                return node, node.next
+            new_head, next_node = reverseList(node.next, n - 1)
+            node.next.next = node
+            node.next = next_node
+            return new_head, next_node
         
-        next_node = sublist_tail.next
-        sublist_tail.next = None
-        reversed_list = self.reverseList(sublist_head)
-        prev.next = reversed_list
-        sublist_head.next = next_node
+        if left == 1:
+            new_head, _ = reverseList(head, right)
+            return new_head
         
-        return dummy.next
-
-    
-    def reverseList(self, head):
-        if not head:
-            return None
-        
-        new_head = head
-        if head.next:
-            new_head = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
-        return new_head
+        head.next = self.reverseBetween(head.next, left - 1, right - 1)
+        return head
