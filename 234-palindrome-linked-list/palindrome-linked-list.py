c876d7a403260824c596deebb1b227b1772e7bc9
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-04 22:04:31 -0700

    Time: 89 ms (6.41%) | Memory: 47.5 MB (5.01%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -6,14 +6,15 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         self.cur = head
-
-        def rec(node):
-            if node is not None:
-                if not rec(node.next):
-                    return False
-                if self.cur.val != node.val:
-                    return False
-                self.cur = self.cur.next
+        return self.rec(head)
+        
+    def rec(self, node):
+        if not node:
             return True
         
-        return rec(head)
+        if not self.rec(node.next):
+            return False
+        if self.cur.val != node.val:
+            return False
+        self.cur = self.cur.next
+        return True
