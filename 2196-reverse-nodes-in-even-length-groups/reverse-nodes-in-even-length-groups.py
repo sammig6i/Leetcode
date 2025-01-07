2fca0419caf527e025712d0e7952eadeaef280a4
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-07 02:51:51 -0700

    Time: 127 ms (25.99%) | Memory: 40.8 MB (14.32%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
@@ -1,34 +1,41 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = head
-        d = 2
-        while prev.next:
-            node = prev
-            n = 0
-            for _ in range(d):
-                if not node.next:
+        def reverse(head: ListNode, k: int) -> ListNode:
+            """Helper function to reverse the first k nodes of a linked list."""
+            prev, curr = None, head
+            for _ in range(k):
+                if not curr:
                     break
-                n += 1
+                next_node = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_node
+            return prev
+
+        def helper(head: ListNode, group_size: int) -> ListNode:
+            """Recursive function to process groups."""
+            # Base case: if there are no more nodes, return
+            if not head:
+                return None
+
+            # Traverse to find the group size
+            node = head
+            count = 0
+            while node and count < group_size:
+                count += 1
                 node = node.next
-            if n & 1:
-                prev = node
+
+            # If group length is even, reverse it
+            if count % 2 == 0:
+                reversed_head = reverse(head, count)
+                head.next = helper(node, group_size + 1)
+                return reversed_head
             else:
-                group_start = prev.next
-                node = prev.next
-                rev = None
-                for _ in range(n):
-                    next_node = node.next
-                    node.next = rev
-                    node, rev = next_node, node
-                
-                next_prev = prev.next
-                group_start.next = node
-                prev.next = rev
-                prev = next_prev
-            d += 1
-        return head
+                # Otherwise, keep it as is and process the next group
+                node = head
+                for _ in range(count - 1):
+                    node = node.next
+                node.next = helper(node.next, group_size + 1)
+                return head
+
+        return helper(head, 1)
