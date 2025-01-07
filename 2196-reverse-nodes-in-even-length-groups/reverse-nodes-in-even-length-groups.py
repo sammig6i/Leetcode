1cd0284848f00f1054c7e20e5e8cfba0c8da2930
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-07 02:12:12 -0700

    Time: 103 ms (66.31%) | Memory: 40.5 MB (46.42%) - LeetSync

---
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = head
+        d = 2
+        while prev.next:
+            node = prev
+            n = 0
+            for _ in range(d):
+                if not node.next:
+                    break
+                n += 1
+                node = node.next
+            if n & 1:
+                prev = node
+            else:
+                node = prev.next
+                rev = None
+                for _ in range(n):
+                    next_node = node.next
+                    node.next = rev
+                    node, rev = next_node, node
+                prev.next.next, prev.next, prev = node, rev, prev.next
+            d += 1
+        return head
