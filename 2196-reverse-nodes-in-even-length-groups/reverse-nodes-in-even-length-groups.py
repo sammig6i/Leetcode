2a51230f71eef4dfea289e605f0a8e0aa2df1070
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-08 17:56:47 -0700

    Time: 95 ms (83.12%) | Memory: 40.6 MB (26.49%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
@@ -1,34 +1,57 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        d = 2
-        prev = head
-        while prev.next:
-            node = prev
-            n = 0
-            for _ in range(d):
-                if not node.next:
-                    break
-                node = node.next
-                n += 1
+    # Function to reverse a linked list
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
+    
+    def reverseEvenLengthGroups(self, head: ListNode) -> ListNode:
+        # Creating a dummy node to avoid adding checks for the first node
+        dummy = ListNode()
+        dummy.next = head
+        
+        prev = dummy
+        # Loop to determine the lengths of groups
+        length = 1
+        while head:
+            tail = head
+            next_head = None
             
-            if n & 1:
-                prev = node
+            # Determining the length of the current group
+            j = 1
+            while j < length and tail and tail.next:
+                tail = tail.next
+                j += 1
+            
+            # Head of the next group
+            next_head = tail.next
+            
+            if j % 2 == 0:
+                # If even-sized group is found
+                # Reversing the group and setting prev and head appropriately
+                tail.next = None
+                prev.next = self.reverseList(head)
+                prev = head
+                head.next = next_head
+                head = next_head
             else:
-                group_start = prev.next
-                cur = prev.next
-                rev = None
-                for _ in range(n):
-                    next_node = cur.next
-                    cur.next = rev
-                    rev, cur = cur, next_node
-                next_prev = prev.next
-                group_start.next = cur
-                prev.next = rev
-                prev = next_prev
-            d += 1
-        return head
+                # If group is odd-sized, simply move to the next group
+                prev = tail
+                head = next_head
+            
+            # Increase the group size
+            length += 1
+        
+        # Returning the head of the modified list
+        return dummy.next
