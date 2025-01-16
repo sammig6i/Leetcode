08093533c20fa6d6b54244034197d7bcb54f5a1c
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-16 00:04:05 -0700

    Time: 0 ms (100.00%) | Memory: 8.2 MB (84.67%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for (int i = 0, j = 0; j < word.size(); ++j) {
+            if (word[j] == ch) {
+                int L = i;
+                int R = j;
+                while (L < R) {
+                    char tmp = word[L];
+                    word[L] = word[R];
+                    word[R] = tmp;
+                    ++L;
+                    --R;
+                }
+                return word;
+            }
+        }
+        return word;
+    }
+};
