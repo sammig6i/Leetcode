3719f1684e6cd38f8371e0d0d95892d2bc0d837f
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-15 22:59:22 -0700

    Time: 0 ms (100.00%) | Memory: 13.4 MB (48.36%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int lastSpaceIdx = -1;
+        int n = s.length();
+        string result;
+        for (int i = 0; i < n; ++i) {
+            if (i == n - 1 || s[i] == ' ') {
+                int reverseIdx = (i == n - 1) ? i : i - 1;
+                for (; reverseIdx > lastSpaceIdx; reverseIdx--) {
+                    result += s[reverseIdx];
+                }
+
+                if (i != n - 1) {
+                    result += ' ';
+                }
+                lastSpaceIdx = i;
+            }
+        }
+        return result;
+    }
+};
