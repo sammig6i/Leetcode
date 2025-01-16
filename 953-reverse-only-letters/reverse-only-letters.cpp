feebab28731c6b44458be93f5250f0e6fa208e6b
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-15 23:42:20 -0700

    Time: 0 ms (100.00%) | Memory: 8 MB (47.84%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
-            while (i < j && !isalpha(s[i])) ++i;
-            while (i < j && !isalpha(s[j])) --j;
-            char tmp = s[i];
-            s[i] = s[j];
-            s[j] = tmp;
+        for (int i = 0, j = s.size() - 1; i < j;) {
+            if (!isalpha(s[i])) {
+                ++i;
+            } else if (!isalpha(s[j])) {
+                --j;
+            } else {
+                char tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+                ++i;
+                --j;
+            }
         }
+
         return s;
     }
 };
