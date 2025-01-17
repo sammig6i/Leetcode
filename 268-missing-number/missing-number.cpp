fbe923cc73b2f0b68bb68fe86ed2e990bf377aab
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-16 18:30:11 -0700

    Time: 19 ms (8.22%) | Memory: 28.1 MB (5.01%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int res = n;
-        for (int i = 0; i < n; ++i) {
-            res ^= i ^ nums[i];
+        unordered_set<int> num_set(nums.begin(), nums.end());
+        for (int i = 0; i < n + 1; ++i) {
+            if (num_set.find(i) == num_set.end()) {
+                return i;
+            }
         }
-        return res;
+
+        return -1;
     }
 };
