241f37e840a9ef59d5b71711f15fbca8d9f0c231
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-16 18:40:05 -0700

    Time: 0 ms (100.00%) | Memory: 21.7 MB (61.81%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,7 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int res = nums.size();
         for (int i = 0; i < nums.size(); ++i) {
-            res += i - nums[i];
+            res ^= i ^ nums[i];
         }
         return res;
     }
