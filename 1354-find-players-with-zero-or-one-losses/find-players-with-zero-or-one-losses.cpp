ef6880257c19cd987bf3d799ef912345a07d9194
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-17 01:40:21 -0700

    Time: 194 ms (54.40%) | Memory: 185.4 MB (37.10%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        vector<vector<int>> res;
         unordered_map<int, int> losses;
-        for (vector<int> match : matches) {
+        for (auto match : matches) {
+            int winner = match[0];
             int loser = match[1];
+
             losses[loser]++;
+            losses[winner] += 0;
         }
 
-        unordered_set<int> no_loss;
-        unordered_set<int> one_loss;
-        for (vector<int> match : matches) {
-            int winner = match[0], loser = match[1];
-            if (losses.find(winner) == losses.end()) {
-                no_loss.insert(winner);
-            } 
-            if (losses.find(loser) != losses.end() && losses[loser] == 1) {
-                one_loss.insert(loser);
+        vector<int> no_loss;
+        vector<int> one_loss;
+        for (const auto& [player, count] : losses) {
+            if (count == 0) {
+                no_loss.push_back(player);
+            } else if (count == 1) {
+                one_loss.push_back(player);
             }
         }
-        
-        vector<int> no_loss_vec(no_loss.begin(), no_loss.end());
-        vector<int> one_loss_vec(one_loss.begin(), one_loss.end());
-        sort(no_loss_vec.begin(), no_loss_vec.end());
-        sort(one_loss_vec.begin(), one_loss_vec.end());
-        return {no_loss_vec, one_loss_vec};
 
+        sort(no_loss.begin(), no_loss.end());
+        sort(one_loss.begin(), one_loss.end());
+        return {no_loss, one_loss};
     }
 };
