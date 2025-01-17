faa16aa7eee2e375caebf3e2e6822d7e0b06d273
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-17 01:55:34 -0700

    Time: 230 ms (41.93%) | Memory: 185.6 MB (36.04%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int, int> losses;
+        unordered_set<int> zero_loss;
+        unordered_set<int> one_loss;
+        unordered_set<int> more_loss;
+
         for (auto match : matches) {
             int winner = match[0];
             int loser = match[1];
 
-            losses[loser]++;
-            losses[winner] += 0;
-        }
+            if (one_loss.find(winner) == one_loss.end() &&
+                more_loss.find(winner) == more_loss.end()) {
+                    zero_loss.insert(winner);
+            } 
 
-        vector<int> no_loss;
-        vector<int> one_loss;
-        for (const auto& [player, count] : losses) {
-            if (count == 0) {
-                no_loss.push_back(player);
-            } else if (count == 1) {
-                one_loss.push_back(player);
+            if (one_loss.find(loser) != one_loss.end()) {
+                one_loss.erase(loser);
+                more_loss.insert(loser);
+            } else if (zero_loss.find(loser) != zero_loss.end()) {
+                zero_loss.erase(loser);
+                one_loss.insert(loser);
+            } else if (more_loss.find(loser) != more_loss.end()) {
+                continue;
+            } else {
+                one_loss.insert(loser);
             }
         }
+        
+        vector<int> zero_loss_vec(zero_loss.begin(), zero_loss.end());
+        sort(zero_loss_vec.begin(), zero_loss_vec.end());
+        vector<int> one_loss_vec(one_loss.begin(), one_loss.end());
+        sort(one_loss_vec.begin(), one_loss_vec.end());
+        return {zero_loss_vec, one_loss_vec};
 
-        sort(no_loss.begin(), no_loss.end());
-        sort(one_loss.begin(), one_loss.end());
-        return {no_loss, one_loss};
     }
 };
