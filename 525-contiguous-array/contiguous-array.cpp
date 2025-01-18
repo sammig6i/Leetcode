bfd7461c2b8c2c297db00f1798c5f2eb2ba65ca2
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-18 15:59:27 -0700

    Time: 8 ms (96.49%) | Memory: 90.5 MB (10.41%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int n = nums.size(), res = 0, count = 0;
-        vector<int> diff_idx(2 * n + 1, -2);
+        vector<int> diff_idx(2 * n + 1, INT_MIN);
         diff_idx[n] = -1;
 
         for (int i = 0; i < n; ++i) {
             count += nums[i] == 1 ? 1 : -1;
-            if (diff_idx[count + n] != -2) {
+            if (diff_idx[count + n] >= -1) {
                 res = max(res, i - diff_idx[count + n]);
             } else {
                 diff_idx[count + n] = i;
             }
         }
-
         return res;
     }
 };
