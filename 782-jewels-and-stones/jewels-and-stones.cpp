48ce3be86a3bb7a48b51b52476dbf61555ceecf6
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-18 20:16:49 -0700

    Time: 1 ms (22.78%) | Memory: 8.6 MB (35.75%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -6,14 +6,10 @@ public:
         for (char j : jewels) {
             jewel_cnt[j]++;           
         }
-        unordered_map<char, int> stone_cnt;
-        for (char s : stones) {
-            stone_cnt[s]++;
-        }
 
-        for (auto& [s, count] : stone_cnt) {
+        for (char s : stones) {
             if (jewel_cnt.find(s) != jewel_cnt.end()) {
-                res += count;
+                res ++;
             }
         }
         return res;
