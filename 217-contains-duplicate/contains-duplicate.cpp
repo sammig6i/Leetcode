11363197862a166714bca5012d066cdab4ee3038
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-18 21:28:05 -0700

    Time: 2 ms (99.74%) | Memory: 61.1 MB (87.82%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int mx = INT_MIN;
         int mn = INT_MAX;
-        for (int i = 0; i < nums.size(); i++) {
+        int mx = INT_MIN;
+        for (int i = 0; i < nums.size(); ++i) {
             if (nums[i] < mn || nums[i] > mx) {
                 mn = min(mn, nums[i]);
                 mx = max(mx, nums[i]);
                 continue;
             }
-            for (int j = 0; j < i; j++) {
-                if (nums[j] == nums[i])
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] == nums[j]) {
                     return true;
+                }
             }
         }
         return false;
