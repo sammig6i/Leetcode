540e9c1744ad80e035ec3cf0e02d3846df314b45
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-20 15:21:48 -0700

    Time: 7 ms (22.57%) | Memory: 10.8 MB (87.44%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> mp;
-        multimap<int, char> r;
+        string res = "";
+        vector<pair<int, char>> freq;
         for (char c : s) {
             mp[c]++;
         }
 
-        for (auto [k, v] : mp) {
-            r.insert({v, k});
+        for (auto [c, count] : mp) {
+            freq.push_back({count, c});
         }
 
-        string res = "";
-        for (auto it = r.rbegin(); it != r.rend(); ++it) {
-            res += string(it->first, it->second);
+        sort(freq.begin(), freq.end());
+        for (int i = freq.size() - 1; i >= 0; --i) {
+            for (int j = 0; j < freq[i].first; ++j) {
+                res += freq[i].second;
+            }
         }
-        return res;
+
+        return res;       
+
     }
 };
