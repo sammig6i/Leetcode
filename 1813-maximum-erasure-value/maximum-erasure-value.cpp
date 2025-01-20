01d2aeb98623a67ba642ef4dbb1e8c053d56676d
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-20 16:49:34 -0700

    Time: 1565 ms (5.11%) | Memory: 131 MB (24.23%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        unordered_map<int, int> mp;
+        unordered_set<int> st;
         int res = 0, L = 0;
         for (int R = 0; R < nums.size(); ++R) {
-            mp[nums[R]]++;
-            while (mp[nums[R]] > 1) {
-                mp[nums[L]]--;
-                if (!mp[nums[L]]) mp.erase(nums[L]);
+            while (st.contains(nums[R])) {
+                st.erase(nums[L]);
                 L++;
             }
-
-            int sum = 0;
-            for (const auto& [n, count] : mp) {
-                sum += n;
-            }
-
-            res = max(res, sum);
+            st.insert(nums[R]);
+            res = max(res, accumulate(st.begin(), st.end(), 0));
         }
         return res;
     }
