dc614920fa92fa983d261ce57083ea0fbd7e7615
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-01-30 01:43:58 -0700

    Time: 0 ms (100.00%) | Memory: 134.3 MB (68.38%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int x = k * 2 + 1;
+        int window_size = 2 * k + 1;
+        long window_sum = 0;
         int n = nums.size();
         vector<int> res(n, -1);
 
-        if (n < x) return res;
-        vector<long> pre(n + 1, 0);
-        for (int i = 0; i < n; ++i) {
-            pre[i + 1] = pre[i] + nums[i];
+        if (n < window_size) {
+            return res;
         }
 
-        for (int i = k; i < n - k; ++i) {
-            long sum = pre[i + k + 1] - pre[i - k];
-            int avg = sum / x;
-            res[i] = avg;
+        for (int i = 0; i < n; ++i) {
+            window_sum += nums[i];
+
+            if (i - window_size >= 0) {
+                window_sum -= nums[i - window_size];
+            }
+
+            if (i >= window_size - 1) {
+                res[i - k] = window_sum / window_size;
+            }
         }
 
         return res;
