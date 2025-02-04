cd615a6c811a528ebf38e136c9e1780d84360f07
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-02-03 17:26:57 -0700

    Time: 0 ms (100.00%) | Memory: 8.2 MB (96.78%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -3,14 +3,8 @@ public:
     string makeGood(string s) {
         string res = "";
         for (char c : s) {
-            if (!res.empty() && tolower(c) == tolower(res.back())) {
-                if (isupper(c) && islower(res.back())) {
-                    res.pop_back();
-                } else if (islower(c) && isupper(res.back())) {
-                    res.pop_back();
-                } else {
-                    res += c;
-                }
+            if (!res.empty() && abs(res.back() - c) == 32) {
+                res.pop_back();
             } else {
                 res += c;
             }
