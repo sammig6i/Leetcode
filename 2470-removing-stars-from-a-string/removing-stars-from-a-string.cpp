50ccc99399e3e874ba12f0a1338456981c121a86
Sammi Ghazzawi <swghazzawi@gmail.com>
2025-02-04 15:46:09 -0700

    Time: 20 ms (48.36%) | Memory: 28.4 MB (72.45%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string res = "";
+        for (char c : s) {
+            if (c == '*') {
+                res.pop_back();
+            } else {
+                res.push_back(c);
+            }
+        }
+
+        return res;
+    }
+};
